e1595308c9c95c3e1c53864d260ab514dd70a157
Ondrej Lukasek <ondrej@lukasek.cz>
2023-11-10 22:03:40 +0100

    Major DB update

--- database.sql
+++ database.sql
@@ -1,70 +1,36 @@
+DROP TABLE Osoba_predmet;
+DROP TABLE Rozvrh_aktivita;
+DROP TABLE `Vyuk_aktivita`;
+DROP TABLE `Mistnost`;
+DROP TABLE `Rozvrh`;
+DROP TABLE `Osoba`;
+DROP TABLE `Predmet`;
+
+CREATE TABLE `Osoba` (
+	`ID_Osoba` INT(5) NOT NULL AUTO_INCREMENT,
+	`jmeno` varchar(15) NOT NULL,
+	`prijmeni` varchar(15) NOT NULL,
+	`email` varchar(20) NOT NULL,
+	`telefon` INT(9) NOT NULL,
+    `role` varchar(4) NOT NULL,
+	PRIMARY KEY (`ID_Osoba`)
+);
+
 CREATE TABLE `Predmet` (
 	`zkratka` varchar(3) NOT NULL,
 	`nazev` varchar(25) NOT NULL,
 	`anotace` varchar(200) NOT NULL,
 	`pocet_kreditu` INT(1) NOT NULL,
 	`typ_ukonceni` varchar(6) NOT NULL,
-	`garant` INT(5) NOT NULL,
-	`vyucujici` INT(5) NOT NULL,
-	`student` INT(5) NOT NULL,
-	`admin` INT(5) NOT NULL,
-	`vyuk_aktivita` INT(5) NOT NULL,
 	PRIMARY KEY (`zkratka`)
 );
 
-CREATE TABLE `Garant` (
-	`ID_Garant` INT(5) NOT NULL AUTO_INCREMENT,
-	`jmeno` varchar(15) NOT NULL,
-	`prijmeni` varchar(15) NOT NULL,
-	`email` varchar(20) NOT NULL,
-	`telefon` INT(9) NOT NULL,
-	PRIMARY KEY (`ID_Garant`)
-);
-
-CREATE TABLE `Vyucujici` (
-	`ID_Vyuc` INT(5) NOT NULL AUTO_INCREMENT,
-	`jmeno` varchar(15) NOT NULL,
-	`prijmeni` varchar(15) NOT NULL,
-	`email` varchar(20) NOT NULL,
-	`telefon` INT(9) NOT NULL,
-	PRIMARY KEY (`ID_Vyuc`)
-);
-
-CREATE TABLE `Student` (
-	`ID_Stud` INT(5) NOT NULL AUTO_INCREMENT,
-	`jmeno` varchar(15) NOT NULL,
-	`prijmeni` varchar(15) NOT NULL,
-	`email` varchar(20) NOT NULL,
-	`telefon` INT(9) NOT NULL,
-	PRIMARY KEY (`ID_Stud`)
-);
-
-CREATE TABLE `Rozvrhar` (
-	`ID_Rozvrhar` INT(5) NOT NULL AUTO_INCREMENT,
-	`jmeno` varchar(15) NOT NULL,
-	`prijmeni` varchar(15) NOT NULL,
-	`email` varchar(20) NOT NULL,
-	`telefon` INT(9) NOT NULL,
-	PRIMARY KEY (`ID_Rozvrhar`)
-);
-
-CREATE TABLE `Administrator` (
-	`ID_Admin` INT(5) NOT NULL AUTO_INCREMENT,
-	`jmeno` varchar(15) NOT NULL,
-	`prijmeni` varchar(15) NOT NULL,
-	`email` varchar(20) NOT NULL,
-	`telefon` INT(9) NOT NULL,
-	PRIMARY KEY (`ID_Admin`)
-);
-
-CREATE TABLE `Vyuk_aktivita` (
-	`ID_Aktiv` INT(5) NOT NULL AUTO_INCREMENT,
-	`typ` varchar(10) NOT NULL,
-	`delka` varchar(10) NOT NULL,
-	`popis` varchar(100) NOT NULL,
-	`opakovani` varchar(20) NOT NULL,
-	`mistnost` INT(5) NOT NULL,
-	PRIMARY KEY (`ID_Aktiv`)
+CREATE TABLE Osoba_predmet (
+    `ID_Osoba` INT(5) NOT NULL,
+    `zkratka` varchar(3) NOT NULL,
+    PRIMARY KEY (ID_Osoba, zkratka),
+    FOREIGN KEY (ID_Osoba) REFERENCES Osoba(ID_Osoba),
+    FOREIGN KEY (zkratka) REFERENCES Predmet(zkratka)
 );
 
 CREATE TABLE `Mistnost` (
@@ -74,38 +40,38 @@ CREATE TABLE `Mistnost` (
 	`popis` varchar(100) NOT NULL,
 	`umisteni` varchar(20) NOT NULL,
 	`admin` INT(5) NOT NULL,
-	PRIMARY KEY (`ID_mist`)
+	PRIMARY KEY (`ID_mist`),
+    FOREIGN KEY (admin) REFERENCES Osoba(ID_Osoba)
+);
+
+CREATE TABLE `Vyuk_aktivita` (
+	`ID_Aktiv` INT(5) NOT NULL AUTO_INCREMENT,
+	`typ` varchar(10) NOT NULL,
+	`delka` varchar(10) NOT NULL,
+	`popis` varchar(100) NOT NULL,
+	`opakovani` varchar(20) NOT NULL,
+	`mistnost` INT(5) NOT NULL,
+    `predmet` varchar(3) NOT NULL,
+	PRIMARY KEY (`ID_Aktiv`),
+    FOREIGN KEY (`predmet`) REFERENCES Predmet(zkratka),
+    FOREIGN KEY (mistnost) REFERENCES Mistnost(ID_mist)
 );
 
 CREATE TABLE `Rozvrh` (
 	`ID_Rozvrh` INT(5) NOT NULL AUTO_INCREMENT,
-	`vyuk_aktivita` INT(5) NOT NULL,
-	`student` INT(5) NOT NULL,
-	`vyucujici` INT(5) NOT NULL,
+	`student` INT(5),
+	`vyucujici` INT(5),
 	`rozvrhar` INT(5) NOT NULL,
-	PRIMARY KEY (`ID_Rozvrh`)
+	PRIMARY KEY (`ID_Rozvrh`),
+    FOREIGN KEY (student) REFERENCES Osoba(ID_Osoba),
+    FOREIGN KEY (vyucujici) REFERENCES Osoba(ID_Osoba),
+    FOREIGN KEY (rozvrhar) REFERENCES Osoba(ID_Osoba)
 );
 
-ALTER TABLE `Predmet` ADD CONSTRAINT `Predmet_fk0` FOREIGN KEY (`garant`) REFERENCES `Garant`(`ID_Garant`);
-
-ALTER TABLE `Predmet` ADD CONSTRAINT `Predmet_fk1` FOREIGN KEY (`vyucujici`) REFERENCES `Vyucujici`(`ID_Vyuc`);
-
-ALTER TABLE `Predmet` ADD CONSTRAINT `Predmet_fk2` FOREIGN KEY (`student`) REFERENCES `Student`(`ID_Stud`);
-
-ALTER TABLE `Predmet` ADD CONSTRAINT `Predmet_fk3` FOREIGN KEY (`admin`) REFERENCES `Administrator`(`ID_Admin`);
-
-ALTER TABLE `Predmet` ADD CONSTRAINT `Predmet_fk4` FOREIGN KEY (`vyuk_aktivita`) REFERENCES `Vyuk_aktivita`(`ID_Aktiv`);
-
-ALTER TABLE `Vyuk_aktivita` ADD CONSTRAINT `Vyuk_aktivita_fk0` FOREIGN KEY (`mistnost`) REFERENCES `Mistnost`(`ID_mist`);
-
-ALTER TABLE `Mistnost` ADD CONSTRAINT `Mistnost_fk0` FOREIGN KEY (`admin`) REFERENCES `Administrator`(`ID_Admin`);
-
-ALTER TABLE `Rozvrh` ADD CONSTRAINT `Rozvrh_fk0` FOREIGN KEY (`vyuk_aktivita`) REFERENCES `Vyuk_aktivita`(`ID_Aktiv`);
-
-ALTER TABLE `Rozvrh` ADD CONSTRAINT `Rozvrh_fk1` FOREIGN KEY (`student`) REFERENCES `Student`(`ID_Stud`);
-
-ALTER TABLE `Rozvrh` ADD CONSTRAINT `Rozvrh_fk2` FOREIGN KEY (`vyucujici`) REFERENCES `Vyucujici`(`ID_Vyuc`);
-
-ALTER TABLE `Rozvrh` ADD CONSTRAINT `Rozvrh_fk3` FOREIGN KEY (`rozvrhar`) REFERENCES `Rozvrhar`(`ID_Rozvrhar`);
-
-SELECT * FROM Administrator;
+CREATE TABLE `Rozvrh_aktivita` (
+    `ID_Rozvrh` INT(5) NOT NULL,
+    `ID_Aktiv` INT(5) NOT NULL,
+    PRIMARY KEY (ID_Rozvrh, ID_Aktiv),
+    FOREIGN KEY (ID_Rozvrh) REFERENCES Rozvrh(ID_Rozvrh),
+    FOREIGN KEY (ID_Aktiv) REFERENCES Vyuk_aktivita(ID_Aktiv)
+);
