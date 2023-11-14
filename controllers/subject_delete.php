28fd58fa67b168daaee917c1f78225300be4f946
Ondrej <xkouma02@stud.fit.vutbr.cz>
2023-11-14 13:40:16 +0100

    fix: remove echo from subject_delete

--- controllers/subject_delete.php
+++ controllers/subject_delete.php
@@ -2,9 +2,7 @@
 
 require_once "../services/subject_service.php";
 
-echo "jsem tu";
 if (isset($_POST["zkratka"])) {
-    echo "jsem tu 2";
     $service = new subjectService();
     $service->deleteSubject($_POST["zkratka"]);
     header("Location: ../views/subject_management.php");
