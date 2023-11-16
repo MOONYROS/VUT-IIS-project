506b20f099aac6b7486279676def3a77f8ac94e8
Ondrej Lukasek <ondrej@lukasek.cz>
2023-11-16 11:23:20 +0100

    Registering and unregistering subject again.

--- controllers/register_subjects.php
+++ controllers/register_subjects.php
@@ -3,4 +3,12 @@ require_once "../services/user_service.php";
 require_once "../services/registration_service.php";
 require_once "../common.php";
 
+$registrationService = new registrationService();
+
+$old = $registrationService->retrieveRegistered($_SESSION['user_id']);
+$new = $registrationService->newlyRegistered($_POST);
+
+$final = $registrationService->unregisterSubject($old, $new);
+$final = $registrationService->registerNewSubject($final, $new);
+
 echo "Subjects successfully registered!";
