7489ee4e9865298f2febf5d3be1ae8b37a7475e5
Ondřej <xkouma02@vutbr.cz>
2023-11-24 18:09:21 +0100

    feat: add simple annotation to adding teachers view

--- views/subject_views/subject_info_teacher.php
+++ views/subject_views/subject_info_teacher.php
@@ -81,5 +81,23 @@ else { ?>
 
 <p><?php if (isset($_GET["message"])) echo $_GET["message"]; ?></p>
 
-
+<h1><?= $_GET["zkratka"] ?></h1>
+<h3> Název</h3>
+<p> <?= $infoArray["nazev"] ?> </p>
+<h3> Anotace</h3>
+<p> <?= $infoArray["anotace"] ?> </p>
+<h3> Počet kreditů</h3>
+<p> <?= $infoArray["pocet_kreditu"] ?> </p>
+<h3> Typ ukončení</h3>
+<p> <?= $infoArray["typ_ukonceni"] ?> </p>
+<h3> Garant</h3>
+<p> <?= "{$infoArray["jmeno"]} {$infoArray["prijmeni"]}" ?> </p>
+<h3> Vyučující</h3>
+<?php
+$finalValue = "";
+foreach ($subjectTeachers as $teacher) {
+    $finalValue = $finalValue . "{$teacher["jmeno"]} {$teacher["prijmeni"]}<br>";
+}
+echo $finalValue;
+?>
 
